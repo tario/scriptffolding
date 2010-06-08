54d88becc2dfaa04f89cb0b8c21e63398d94d637
Dario Seminara <rseminara@hotmail.com>
2010-06-08 11:52:41 -0300

    fixed project name in license banners of rb sources under lib/ directory

--- lib/scriptffolding/view_helpers.rb
+++ lib/scriptffolding/view_helpers.rb
@@ -1,23 +1,25 @@
 =begin
 
-This file is part of the negai project, http://github.com/tario/negai
+This file is part of the scriptffolding project, http://github.com/tario/scriptffolding
 
 Copyright (c) 2009-2010 Roberto Dario Seminara <robertodarioseminara@gmail.com>
 
-negai is free software: you can redistribute it and/or modify
+scriptffolding is free software: you can redistribute it and/or modify
 it under the terms of the gnu general public license as published by
 the free software foundation, either version 3 of the license, or
 (at your option) any later version.
 
-negai is distributed in the hope that it will be useful,
+scriptffolding is distributed in the hope that it will be useful,
 but without any warranty; without even the implied warranty of
 merchantability or fitness for a particular purpose.  see the
 gnu general public license for more details.
 
 you should have received a copy of the gnu general public license
-along with negai.  if not, see <http://www.gnu.org/licenses/>.
+along with scriptffolding.  if not, see <http://www.gnu.org/licenses/>.
 
 =end
+
+
 require "app/helpers/scriptffold_helper.rb"
 
 ActionView::Base.class_eval do
